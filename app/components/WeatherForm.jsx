7d655d9e2cafec731738be1dc95710ac957626e9
Dustin Morris <dwmorris.biz@outlook.com>
2016-10-10 13:12:10 -0500

    refactored using arrow functions

--- app/components/WeatherForm.jsx
+++ app/components/WeatherForm.jsx
@@ -15,7 +15,7 @@ var WeatherForm = React.createClass({
     return (
         <div>
           <form onSubmit={this.onFormSubmit}>
-            <input type="text" ref="location"/>
+            <input type="text" ref="location" placeholder="Nashville, TN"/>
             <button>Get Weather</button>
           </form>
         </div>

cb35a6bb236662fa2036b6d3307caa83fcdcce5c
Dustin Morris <dwmorris.biz@outlook.com>
2016-10-11 00:39:24 -0500

    styles for about and basic content

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,13 +3,24 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h3>About React Weather</h3>
+      <h1 className="text-center">About React Weather</h1>
       <p>This is an app built with node.js and react.js. Weather information
         is pulled from the Open Weather API by user request and the resulting
         weather is displayed for the user in a message just below the search box.
       </p>
       <p>Built by Dustin Morris 2016.</p>
-      <span><a href="http://www.designbysparticus.com">designbysparticus</a></span>
+
+
+      <span>
+        <h3>Links</h3>
+        <ul>
+          <li><a href="https://github.com/dwmorrisdev/ReactWeather">ReactWeather on Github</a></li>
+          <li><a href="https://openweathermap.org/">OpenWeatherMap.org</a></li>
+          <li><a href="https://facebook.github.io/react/">React</a></li>
+          <li>Photos by <a href="https://unsplash.com/">Unsplash</a></li>
+          <li><a href="http://www.designbysparticus.com">designbysparticus</a></li>
+        </ul>
+        </span>
 
     </div>
   )
